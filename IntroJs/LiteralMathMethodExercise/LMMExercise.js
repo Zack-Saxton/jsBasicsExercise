eaeeb9aa541b518048ad2656c208ff9be10872d5
Zack Saxton <zacksaxton@SCWDBs-MacBook-Pro.local>
2021-02-25 10:11:20 -0600

    compeleted exercise had to get some answers

---
+++ IntroJs/LiteralMathMethodExercise/LMMExercise.js
@@ -0,0 +1,55 @@
+ let warmHugs = "Hi, I'm Olaf and I like warm hugs.";
+
+// console.log(warmHugs.toUpperCase());
+
+
+// why can't I change it using let
+warmHugs = warmHugs.replace("like", "love");
+console.log(warmHugs);
+
+let impaled = "Oh, look at that. I've been impaled.";
+
+console.log(impaled.slice(18));
+
+let dotDotDot = "...";
+
+let skullBones = `I don't have a skull ${dotDotDot} or bones.`;
+
+console.log(skullBones);
+
+console.log(Math.PI);
+
+let randomNumber = Math.random();
+randomNumber *= 3;
+randomNumber = Math.floor(randomNumber);
+randomNumber ++;
+
+console.log(randomNumber);
+
+console.log(randomNumber);
+
+
+console.log("Let it go!... ".repeat(3).trim() .toUpperCase());
+
+// let reindeers = "Reindeers are better than people.";
+
+
+
+// console.log(reindeers.replaceAll(" ", "_"));
+
+console.log(Math.sqrt(2));
+
+let newRandomNumber = Math.random();
+newRandomNumber *= 17;
+newRandomNumber = Math.floor(newRandomNumber);
+newRandomNumber += 7;
+
+console.log(newRandomNumber);
+
+
+
+
+
+
+
+
